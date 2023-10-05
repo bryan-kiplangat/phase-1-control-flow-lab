f13598281e4cb32b4cb46bc0afa27a1f729d03a2
bryan-kiplangat <kipbryan.bett@gmail.com>
2023-10-05 10:29:54 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let result = ""
+
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    result = "This one is on me!"
+  } else if (ride > 200 && ride <= 2500 ) {
+    result = 'I will gladly take your thirty bucks.'
+  } else if (ride > 2500) {
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination) {
+
+  return result = (destination === "NYC") ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.';
+      break;
+  }
+
+  return result
 }
+
